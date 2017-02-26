e4305fd7893d3b4fd35c666f232ae0b5fe51f14e
menjaw <menjaholm@gmail.com>
2017-02-26 17:54:14 +0100

    Facade and method heads added

---
+++ SP3/PersistenceStudentGrades/src/main/java/domain/Facade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author Menja
+ */
+public class Facade {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void add() {
+
+    }
+
+    public static void edit() {
+
+    }
+
+    public static void find() {
+
+    }
+
+    public static void delete() {
+    }
+}
