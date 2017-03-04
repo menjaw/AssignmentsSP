b3fdaadc60bcac7f1a855805a5a3182db834ae09
menjaw <menjaholm@gmail.com>
2017-03-04 19:46:51 +0100

    assignment 4 almost done

--- SP4/AjaxCallsJsExercise3and4_Day3/src/main/webapp/TimeHandler.js
+++ SP4/AjaxCallsJsExercise3and4_Day3/src/main/webapp/TimeHandler.js
@@ -3,16 +3,50 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/*ASSIGNMENT 3*/
 //declare variable to touch DOM-structure
 var showTime = document.getElementById("showTime");
-
 
 var date = new Date();//now it takes the browsers timezone
 var hour = date.getHours();
 var min = date.getMinutes();
 var sec = date.getSeconds();
 
-
 showTime.innerHTML = "The current time is: " + hour + ":" + min + ":" + sec;
 console.log(showTime);
+
+/*ASSIGNMENT 4*/
+//var heartDirection = document.getElementById("heartDirection");
+
+//var north = document.getElementById("north");
+//var south= document.getElementById("south");
+//var east = document.getElementById("east");
+//var west = document.getElementById("west");
+
+document.getElementById("north").onclick = function() {myFunction()};
+function myFunction() {
+    document.getElementById("heartDirections").innerHTML = "North";
+};
+
+document.getElementById("north").onclick = function() {myFunction()};
+function myFunction() {
+    document.getElementById("heartDirections").innerHTML = "South";
+};
+
+document.getElementById("north").onclick = function() {myFunction()};
+function myFunction() {
+    document.getElementById("heartDirections").innerHTML = "East";
+};
+
+document.getElementById("north").onclick = function() {myFunction()};
+function myFunction() {
+    document.getElementById("heartDirections").innerHTML = "West";
+};
+ 
+
+
+
+
+
 
