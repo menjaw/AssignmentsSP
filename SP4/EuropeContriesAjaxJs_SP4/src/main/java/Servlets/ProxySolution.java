ef9aedd98f3c6eabc1003b45cf9ff76ef59d98da
menjaw <menjaholm@gmail.com>
2017-03-04 12:29:44 +0100

    init SP4 commit

---
+++ SP4/EuropeContriesAjaxJs_SP4/src/main/java/Servlets/ProxySolution.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Menja
+ */
+@WebServlet(name = "ProxySolution", urlPatterns = {"/ProxySolution"})
+public class ProxySolution extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        //refererer til js filens URL efter ?
+        String getCodes = request.getParameter("codes");
+        
+        
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+//Cross Origin Calls er når man laver et request fra en hjemmeside til en anden hjemmeside (Origin er hjemmeside/ip-addresse)(opstår fra browsere)
+//Problemet opstår hvis serveren ikke er opsat til at måtte sende et response på et Cross origin Request.
+//Lav et mellemled (proxy), der opstår ikke Cross Origin hvis der foregår fra en server
+//
---
+++ SP4/EuropeContriesAjaxJs_SP4/src/main/webapp/EventHandler.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var map = document.getElementById("svg2");
+map.addEventListener("click", function (event) {
+    //får printet landekoden ud
+    console.log(event.target.id);
+
+
+//henter data fra en server på en hjemmeside
+    var url = "http://restcountries.eu/rest/v1/alpha?codes=" + event.target.id;
+    var conf = {method: 'get'};//AJAX er når man kalder
+    var promise = fetch(url, conf);
+    promise.then(function (response) {
+        return response.text();
+    }).then(function (text) {
+        var country = JSON.parse(text);
+        var name = document.getElementById("name");
+        var population = document.getElementById("population");
+        var area = document.getElementById("area");
+
+        name.innerHTML = "Country: " + country[0].name;
+        population.innerHTML = "Area: " + country[0].area + " Kvm";
+        area.innerHTML = "Population: " + country[0].population + " Millions";
+        //console.log(country);
+    });
+});
+
+
